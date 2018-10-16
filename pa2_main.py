8813bc1a462d3d0d41fb25f9c2a497533dc8c0bf
Andyngollo <43942913+Andyngollo@users.noreply.github.com>
2018-10-16 00:06:49 -0600

    Update pa2_main.py

--- pa2_main.py
+++ pa2_main.py
@@ -1,15 +1,10 @@
 if __name__ == "__main__":
-    cap_join = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
-    print(cap_join)
-    def cap_join(string_list):
-        cap_join = ["calvin"]
-        new_list = cap_join
-        for i in string_list:
-            new_list.append(i[:1].upper() + i[1:])
-        return "".join(new_list)
-
-
-
+    list = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
+    def cap_join(list):
+        new_list =" ".join(list)
+        new_list = new_list.title()
+        return new_list
+    print(cap_join(list))
 
 
 
